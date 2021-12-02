f68c6f9c2367bed91c0b703ee8f4cb0e27b1e8ea
joyce pinilla <joyce.pinilla@live.com>
2021-12-01 20:24:41 -0700

    eighth commit

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,22 @@
-function EnterName() {
-    var name = prompt("hi, what's your name?");
-    var isNum = !isNaN(Number(name));
-    if (!isNum) {
-      alert("thanks for visiting my page, " + name + "! I hope you enjoy your visit :)");
-    } else {
-      alert("you didn't tell me your name :(");
-    }
-    return name;
+(function () {
+  const name = prompt("hi! what's your name?")
+  const date = new Date()
+  const hour = date.getHours()
+
+  let greeting;
+  let bye;
+
+  if (hour >= 5 && hour < 12) {
+    greeting = 'good morning,'
+    bye = 'enjoy your morning!'
+  } else if (hour >= 12 && hour < 18) {
+    greeting = 'good afternoon,'
+    bye = 'enjoy your afternoon!'
+  } else {
+    greeting = 'good evening,'
+    bye = 'enjoy your night!'
   }
-  console.log(EnterName());
+
+  alert(`${greeting} ${name}! 
+  thanks for visiting my page, ${bye}`)
+}());
